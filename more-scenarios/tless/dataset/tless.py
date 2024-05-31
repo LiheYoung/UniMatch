723389f34677df43bda309d9a06ab555ee2a34a9
JoHub <j.a.huber@t-online.de>
2024-05-31 14:58:08 +0200

    reimplemented split handling and dataloading (including lots of logging)

--- more-scenarios/tless/dataset/tless.py
+++ more-scenarios/tless/dataset/tless.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import os
 import glob
@@ -8,6 +9,8 @@ from dataset.transform import *
 import torch
 from torch.utils.data import Dataset, ConcatDataset
 from torchvision import transforms
+
+from util.utils import init_log
 
 
 # split should be between 0 and 1 indicating percentage of labeled data
@@ -40,71 +43,65 @@ class TlessDataset(Dataset):
         if self.dataset not in ['train_pbr', 'test_primesense', 'train_primesense', 'train_render_reconst']:
             raise ValueError(f'Invalid split: {self.dataset}')
 
+        logger = init_log('tless', logging.INFO)
+        if not logger:
+            logger = logging.getLogger('tless')
+
+        logger.info(f"Reading in txt file: {txt_file}")
+
         if mode == 'train_l' or mode == 'train_u':
-            self.ids = self.read_txt_file(txt_file)
+            with open(txt_file, 'r') as f:
+                self.ids = f.read().splitlines()
             if mode == 'train_l' and nsample is not None:
                 self.ids *= math.ceil(nsample / len(self.ids))
                 self.ids = self.ids[:nsample]
         else:
-            self.ids = self.read_txt_file(f"splits/{dataset}.txt")
+            with open(f"splits/{dataset}.txt", 'r') as f:
+                self.ids = f.read().splitlines()
 
-        self.masks = self.collect_masks()
+        logger.info(f"Finished loading txt, Number of images: {len(self.ids)}, First: {self.ids[0]}")
 
-        self.scene_gt_infos = list(sorted(glob.glob(os.path.join(self.root, dataset, "*", "scene_gt_info.json"))))
+        logger.info("Loading scene_gt")
         self.scene_gts = list(sorted(glob.glob(os.path.join(self.root, dataset, "*", "scene_gt.json"))))
+        logger.info(f"Finished loading scene_gt, Number of scene_gt: {len(self.scene_gts)}, First: {self.scene_gts[0]}")
 
         self.void_classes = [0]
         self.valid_classes = range(1, 31)  # classes: 30
         self.class_map = dict(zip(self.valid_classes, range(30)))
 
+        logger.info("Caching scene_gt")
         # Cache parsed JSON files
         self.scene_gt_cache = {}
         for scene_gt_path in self.scene_gts:
             with open(scene_gt_path) as f:
                 self.scene_gt_cache[scene_gt_path] = json.load(f)
-
-    def read_txt_file(self, txt_file):
-        ids = []
-        with open(txt_file, 'r') as f:
-            for line in f:
-                scene_id, image_id = line.strip().split()
-                image_ext = "jpg" if self.dataset == 'train_pbr' else "png"
-                img_path = os.path.join(self.root, self.dataset, scene_id, "rgb", f"{image_id}.{image_ext}")
-                ids.append(img_path)
-        return sorted(ids)
-
-
-    def collect_masks(self):
-        masks = {}
-        for img_path in self.ids:
-            scene_id = os.path.basename(os.path.dirname(os.path.dirname(img_path)))
-            im_id = os.path.basename(img_path).split('.')[0]
-            masks_path = os.path.join(self.root, self.dataset, scene_id, "mask_visib")
-            masks_files = sorted([f for f in os.listdir(masks_path) if f.startswith(im_id) and f.endswith('.png')])
-            masks[im_id] = [os.path.join(masks_path, f) for f in masks_files]
-        return masks
+        logger.info("Finished caching scene_gt")
 
     def __getitem__(self, idx):
-        img_path = self.ids[idx]
-        im_id = os.path.basename(img_path).split('.')[0]
-        #im_id = img_path.split('/')[-1].split('.')[0]
-        scene_id = os.path.basename(os.path.dirname(os.path.dirname(img_path)))
-        #scene_id = img_path.split('/')[-3]
+        id = self.ids[idx]
+        img_path = id.split(' ')[0]
+        mask_count = int(id.split(' ')[1])
 
-        img = Image.open(img_path).convert("RGB")
+        path_parts = img_path.split('/')
+
+        scene_id = path_parts[1]
+        img_id = path_parts[3].split('.')[0]
+
+        image_path = os.path.join(self.root, img_path)
+        img = Image.open(image_path).convert("RGB")
 
         # Object ids
         scene_gt_item = self.scene_gts[int(scene_id)] if self.dataset == 'train_pbr' else self.scene_gts[
             int(scene_id) - 1]
-        scene_gt = self.scene_gt_cache[scene_gt_item][str(int(im_id))]
+        scene_gt = self.scene_gt_cache[scene_gt_item][str(int(img_id))]
         obj_ids = [gt['obj_id'] for gt in scene_gt]
 
         # we ignore non-visible objects
 
         # mask_visib
-        masks_visib_path = self.masks[im_id]
-        masks_visib = torch.zeros((len(masks_visib_path), img.size[1], img.size[0]), dtype=torch.uint8)
-        for i, mp in enumerate(masks_visib_path):
+        mask_paths = [os.path.join(self.root, self.dataset, scene_id, "mask_visib", f"{img_id}_{i:06}.png") for i in range(int(mask_count))]
+        masks_visib = torch.zeros((mask_count, img.size[1], img.size[0]), dtype=torch.uint8)
+        for i, mp in enumerate(mask_paths):
             masks_visib[i] = torch.from_numpy(np.array(Image.open(mp).convert("L")))
 
         # create a single label image
--- more-scenarios/tless/unimatch.py
+++ more-scenarios/tless/unimatch.py
@@ -117,7 +117,7 @@ def main():
                 (img_u_w, img_u_s1, img_u_s2, ignore_mask, cutmix_box1, cutmix_box2),
                 (img_u_w_mix, img_u_s1_mix, img_u_s2_mix, ignore_mask_mix, _, _)) in enumerate(loader):
 
-            if rank == 0:
+            if rank == 0 and i % 10 == 0:
                 logger.info('Starting epoch {:}'.format(i))
 
             img_x, mask_x = img_x.cuda(), mask_x.cuda()
