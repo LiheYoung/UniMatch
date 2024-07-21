fix normalization fr

more-scenarios/medical-dlv3+resnet/dataset/transform.py
@@ -53,10 +53,11 @@ def obtain_cutmix_box(img_size, p=0.5, size_min=0.02, size_max=0.4, ratio_1=0.3,
     return mask
 
 def normalize(img, mask=None):
-    img = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ])(img)
+    normalization = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    if len(img.shape) == 4:
+        img = torch.stack([normalization(img[i]) for i in range(img.shape[0])])
+    else:
+        img = normalization(img)
     if mask is not None:
         mask = torch.from_numpy(np.array(mask)).long()
         return img, mask